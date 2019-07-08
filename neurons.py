fbdaad8264c709a895201cb18d50d55fb3abef3c
Steven Bamford <steven@stevenbamford.com>
2019-07-08 18:17:45 +0100

    Improving explanation of neuron model

--- neurons.py
+++ neurons.py
@@ -3,6 +3,7 @@ import brian2 as b2
 
 class DiehlAndCookBaseNeuronGroup(b2.NeuronGroup):
     def __init__(self):
+        self.create_base_namespace()
         self.create_namespace()
         self.create_base_equations()
         self.create_equations()
@@ -18,38 +19,73 @@ class DiehlAndCookBaseNeuronGroup(b2.NeuronGroup):
         )
         self.initialize()
 
+    def create_base_namespace(self):
+        self.namespace = {
+            "v_eqm_synE": 0 * b2.mV,
+            "tau_ge": 1.0 * b2.ms,
+            "tau_gi": 2.0 * b2.ms,
+        }
+
     def create_base_equations(self):
+        """v : membrane potential of each neuron
+                - in the absence of synaptic currents (`I_syn?`), this decays
+                  exponentially to the resting potential (`v_rest_e`) with
+                  time constant `tau_leak_e`
+           I_synE: current due to excitatory synapses
+                - this is proportional to the conductance of the excitatory
+                  synapses (`ge`) and the difference between the membrane
+                  potential (`v`) and the equilibrium potential of the
+                  excitatory synapses (v_eqm_e)
+                - as implemented this implicitly includes a constant factor
+                  of the membrane resistance
+           I_synI: current due to inhibitory synapses
+           ge : conductance of excitatory synapses
+                - this decays with time constant `tau_ge`
+           gi : conductance of inhibitory synapses
+                - this decays with time constant `tau_gi`
+           v_eqm_synI_e: equilibrium potentials of inhibitory synapses in
+                         an excitatory neuron
+           v_eqm_synI_i: equilibrium potentials of inhibitory synapses in
+                         an inhibitory neuron
+           tau_e: time constant for membrane potential in an excitatory neuron
+           tau_i: time constant for membrane potential in an inhibitory neuron
+           tau_ge: conductance time constant for an excitatory synapse
+           tau_gi: conductance time constant for an inhibitory synapse
+        """
         self.model = b2.Equations(
             """
-            I_synE = ge * nS * -v  : amp
-            dge/dt = -ge / (1.0*ms)  : 1
-            dgi/dt = -gi / (2.0*ms)  : 1
+            I_synE = ge * nS * (v_eqm_synE - v)  : amp
+            dge/dt = -ge / tau_ge  : 1
+            dgi/dt = -gi / tau_gi  : 1
             """
         )
 
 
 class DiehlAndCookExcitatoryNeuronGroup(DiehlAndCookBaseNeuronGroup):
+    """Simple model of an excitatory (pyramidal) neuron"""
     def __init__(self, N, test_mode=True):
         self.N = N
         self.test_mode = test_mode
         super().__init__()
 
     def create_namespace(self):
-        self.namespace = {
+        self.namespace.update({
             "v_rest_e": -65.0 * b2.mV,
             "v_reset_e": -65.0 * b2.mV,
             "v_thresh_e": -52.0 * b2.mV,
+            "tau_e": 100.0 * b2.ms,
+            "v_eqm_synI_e": -100.0 * b2.mV,
             "theta_plus_e": 0.05 * b2.mV,
             "theta_init": 20.0 * b2.mV,
             "refrac_e": 5.0 * b2.ms,
             "tc_theta": 1e7 * b2.ms,
-        }
+        })
 
     def create_equations(self):
         self.model += b2.Equations(
             """
-            dv/dt = ((v_rest_e - v) + (I_synE + I_synI) / nS) / (100*ms)  : volt (unless refractory)
-            I_synI = gi * nS * (-100*mV - v)  : amp
+            dv/dt = ((v_rest_e - v) + (I_synE + I_synI) / nS) / tau_e  : volt (unless refractory)
+            I_synI = gi * nS * (v_eqm_synI_e - v)  : amp
             dtimer/dt = 0.1  : second
             """
         )
@@ -74,23 +110,26 @@ class DiehlAndCookExcitatoryNeuronGroup(DiehlAndCookBaseNeuronGroup):
 
 
 class DiehlAndCookInhibitoryNeuronGroup(DiehlAndCookBaseNeuronGroup):
+    """Simple model of an inhibitory (basket) neuron"""
     def __init__(self, N):
         self.N = N
         super().__init__()
 
     def create_namespace(self):
-        self.namespace = {
+        self.namespace.update({
             "v_rest_i": -60.0 * b2.mV,
             "v_reset_i": -45.0 * b2.mV,
             "v_thresh_i": -40.0 * b2.mV,
+            "tau_i": 10.0 * b2.ms,
+            "v_eqm_synI_i": -85.0 * b2.mV,
             "refrac_i": 2.0 * b2.ms,
-        }
+        })
 
     def create_equations(self):
         self.model += b2.Equations(
             """
-            dv/dt = ((v_rest_i - v) + (I_synE + I_synI) / nS) / (10*ms)  : volt (unless refractory)
-            I_synI = gi * nS * (-85.*mV-v)  : amp
+            dv/dt = ((v_rest_i - v) + (I_synE + I_synI) / nS) / tau_i  : volt (unless refractory)
+            I_synI = gi * nS * (v_eqm_synI_i - v)  : amp
             """
         )
 
