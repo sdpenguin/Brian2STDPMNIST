9ba79cc206612b2d0dca4a530f2a7b52cde24940
Steven Bamford <steven@stevenbamford.com>
2019-07-09 15:32:01 +0100

    Improve organisation and documentation of neurons

--- neurons.py
+++ neurons.py
@@ -54,7 +54,9 @@ class DiehlAndCookBaseNeuronGroup(b2.NeuronGroup):
         """
         self.model = b2.Equations(
             """
+            dv/dt = ((v_rest - v) + (I_synE + I_synI) / nS) / tau  : volt (unless refractory)
             I_synE = ge * nS * (v_eqm_synE - v)  : amp
+            I_synI = gi * nS * (v_eqm_synI - v)  : amp
             dge/dt = -ge / tau_ge  : 1
             dgi/dt = -gi / tau_gi  : 1
             """
@@ -63,31 +65,39 @@ class DiehlAndCookBaseNeuronGroup(b2.NeuronGroup):
 
 class DiehlAndCookExcitatoryNeuronGroup(DiehlAndCookBaseNeuronGroup):
     """Simple model of an excitatory (pyramidal) neuron"""
+
     def __init__(self, N, test_mode=True):
         self.N = N
         self.test_mode = test_mode
         super().__init__()
 
     def create_namespace(self):
-        self.namespace.update({
-            "v_rest_e": -65.0 * b2.mV,
-            "v_reset_e": -65.0 * b2.mV,
-            "v_thresh_e": -52.0 * b2.mV,
-            "tau_e": 100.0 * b2.ms,
-            "v_eqm_synI_e": -100.0 * b2.mV,
-            "theta_plus_e": 0.05 * b2.mV,
-            "theta_init": 20.0 * b2.mV,
-            "refrac_e": 5.0 * b2.ms,
-            "tc_theta": 1e7 * b2.ms,
-        })
+        self.namespace.update(
+            {
+                "v_rest_e": -65.0 * b2.mV,
+                "v_reset_e": -65.0 * b2.mV,
+                "v_thresh_e": -52.0 * b2.mV,
+                "tau_e": 100.0 * b2.ms,
+                "v_eqm_synI_e": -100.0 * b2.mV,
+                "theta_plus_e": 0.05 * b2.mV,
+                "theta_init": 20.0 * b2.mV,
+                "refrac_e": 5.0 * b2.ms,
+                "tc_theta": 1e7 * b2.ms,
+            }
+        )
 
     def create_equations(self):
         self.model += b2.Equations(
             """
-            dv/dt = ((v_rest_e - v) + (I_synE + I_synI) / nS) / tau_e  : volt (unless refractory)
-            I_synI = gi * nS * (v_eqm_synI_e - v)  : amp
             dtimer/dt = 0.1  : second
             """
+        )
+        # This timer seems a bit odd: it increases more slowly than the regular
+        # simulation time, and is only used in the threshold code, to prevent spikes.
+        # It effectively increase the refractory time affecting spikes (but not dv/dt)
+        # by a factor of 10 (to biologically unrealistic values).
+        self.model = b2.Equations(
+            str(self.model), v_rest="v_rest_e", tau="tau_e", v_eqm_synI="v_eqm_synI_e"
         )
         if self.test_mode:
             self.model += b2.Equations("theta  : volt")
@@ -105,32 +115,33 @@ class DiehlAndCookExcitatoryNeuronGroup(DiehlAndCookBaseNeuronGroup):
             self.reset += "; theta += theta_plus_e"
 
     def initialize(self):
+        # I am not sure why this value is chosen for the initial membrane potential
         self.v = self.namespace["v_rest_e"] - 40.0 * b2.mV
         self.theta = self.namespace["theta_init"]
 
 
 class DiehlAndCookInhibitoryNeuronGroup(DiehlAndCookBaseNeuronGroup):
     """Simple model of an inhibitory (basket) neuron"""
+
     def __init__(self, N):
         self.N = N
         super().__init__()
 
     def create_namespace(self):
-        self.namespace.update({
-            "v_rest_i": -60.0 * b2.mV,
-            "v_reset_i": -45.0 * b2.mV,
-            "v_thresh_i": -40.0 * b2.mV,
-            "tau_i": 10.0 * b2.ms,
-            "v_eqm_synI_i": -85.0 * b2.mV,
-            "refrac_i": 2.0 * b2.ms,
-        })
+        self.namespace.update(
+            {
+                "v_rest_i": -60.0 * b2.mV,
+                "v_reset_i": -45.0 * b2.mV,
+                "v_thresh_i": -40.0 * b2.mV,
+                "tau_i": 10.0 * b2.ms,
+                "v_eqm_synI_i": -85.0 * b2.mV,
+                "refrac_i": 2.0 * b2.ms,
+            }
+        )
 
     def create_equations(self):
-        self.model += b2.Equations(
-            """
-            dv/dt = ((v_rest_i - v) + (I_synE + I_synI) / nS) / tau_i  : volt (unless refractory)
-            I_synI = gi * nS * (v_eqm_synI_i - v)  : amp
-            """
+        self.model = b2.Equations(
+            str(self.model), v_rest="v_rest_i", tau="tau_i", v_eqm_synI="v_eqm_synI_i"
         )
 
         self.threshold = "v > v_thresh_i"
