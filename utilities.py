sdpenguin/Brian2STDPMNIST

commit e3a2a141a037544b55cfc46ff9b0e52b54c3b7c8

Use all examples for accuracy calculation when using pre-existing assignments

utilities.py
@@ -430,14 +430,20 @@ def get_windows(nseen, progress_assignments_window, progress_accuracy_window):
         log.debug(
             "Fewer examples have been seen than required for the requested progress windows."
         )
-        log.debug(
-            "Discarding first 20% of available examples to avoid initial contamination."
-        )
-        log.debug("Dividing remaining examples in proportion to requested windows.")
-        assignments_window = int(
-            0.8 * nseen * progress_assignments_window / progress_window
-        )
-        accuracy_window = int(0.8 * nseen * progress_accuracy_window / progress_window)
+        if progress_assignments_window > 0:
+            log.debug(
+                "Discarding first 20% of available examples to avoid initial contamination."
+            )
+            log.debug("Dividing remaining examples in proportion to requested windows.")
+            assignments_window = int(
+                0.8 * nseen * progress_assignments_window / progress_window
+            )
+            accuracy_window = int(
+                0.8 * nseen * progress_accuracy_window / progress_window
+            )
+        else:
+            # if requested accuracy window is zero, then implies test mode: use all seen
+            accuracy_window = nseen
     else:
         assignments_window = progress_assignments_window
         accuracy_window = progress_accuracy_window

commit ab7770d1dd7f02abb27c712bbaea5cf6c51c2350

Fix bug introduced in e3a2a14

utilities.py
@@ -442,7 +442,8 @@ def get_windows(nseen, progress_assignments_window, progress_accuracy_window):
                 0.8 * nseen * progress_accuracy_window / progress_window
             )
         else:
-            # if requested accuracy window is zero, then implies test mode: use all seen
+            # if requested assignments window is zero, then implies test mode: use all seen
+            assignments_window = 0
             accuracy_window = nseen
     else:
         assignments_window = progress_assignments_window